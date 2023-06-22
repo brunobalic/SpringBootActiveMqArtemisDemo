04ce0a82eea3184e3585e64f8c1712ceee2fd028
brunobalic <bruno.balic@icloud.com>
2023-06-22 14:11:58 +0200

    Initial commit.

---
+++ src/test/java/com/brunobalic/activemqdemo/ActiveMqDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brunobalic.activemqdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActiveMqDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
